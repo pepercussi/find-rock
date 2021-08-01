1376d3ebde91dd819087fb25225a511447deb738
Guido <guidopardo@gmail.com>
2021-08-01 12:54:23 -0300

    Iteramos PageArtist

--- src/page-artist.js
+++ src/page-artist.js
@@ -2,11 +2,28 @@ import React, { Comment, Component} from 'react';
 
 import SearchBar from './components/search-bar.js'
 import SimilarArtist from './components/similar-artist.js';
+import Loading from './components/loading.js';
+import Error from './components/error.js';
 
 import "./page-artist.css"
 
 class PageArtist extends Component {
-  state = {};
+  state = {
+    data: {
+      artist:{
+        image: [
+          {"#text": ""},
+          {"#text": ""},
+          {"#text": ""},
+          {"#text": ""},
+          {"#text": ""}
+        ],
+        bio:{
+          summary: ""
+        }
+      }
+    }
+  };
   handleChange = e => {
     this.setState({
       [e.target.name]: e.target.value
@@ -14,6 +31,35 @@ class PageArtist extends Component {
       //busqueda: e.target.value
     });
   };
+
+  componentDidMount(){
+    this.fetchData(
+      "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=608225c9fdbbd3af8e7cb331c3a71dfc&format=json"
+    );
+  }
+
+  fetchData = async url =>{
+    this.setState({
+      loading: true
+    });
+    const response = await fetch(url);
+    const data = await response.json();
+    if(data.error){
+      this.setState({
+        loading: false,
+        error: true,
+        errorMensaje: data.message
+      });
+    }else{
+      this.setState({
+        loading: false,
+        error: false,
+        data: data
+      });
+    }
+  };
+
+
   render(){
     return (
       <React.Fragment>
@@ -21,13 +67,15 @@ class PageArtist extends Component {
           onChange={this.handleChange}
           busqueda={ this.state.busqueda }
         />
+        {this.state.loading && <Loading/>}
+        {this.state.error && <Error errorMensaje={this.state.errorMensaje} />}        
         <div className="container">
             <div className="row centrar">
                 <div className="col-md-3"></div>
                 <div className="col-md-6">
-                    <img className="pic-artist margenes50" src="https://cucharasonica.com/files/2013/05/Gustavo_Cerati-Bocanada-Frontal-600x600.jpg" alt="" />
-                    <h2>Gustavo Cerati</h2>
-                    <p>loremp imp sum....</p>
+                    <img className="pic-artist margenes50" src={ this.state.data.artist.image[3]["#text"]} alt="" />
+                    <h2>{ this.state.data.artist.name }</h2>
+                    <p>{ this.state.data.artist.bio.summary }</p>
                     
                 </div>
 
