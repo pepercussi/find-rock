abf398718e6ba8316ae776352901b55b0cdecd75
Guido <guidopardo@gmail.com>
2021-08-01 09:50:19 -0300

    History.push

--- src/page-home.js
+++ src/page-home.js
@@ -3,6 +3,19 @@ import "./page-home.css";
 import logo from './logo.svg';
 
 class PageHome extends React.Component{
+    handleSubmit = e =>{
+        e.preventDefault();
+        this.props.history.push("/busqueda?"+this.state.busqueda);
+    }
+    onChange = e =>{
+        //console.log(e.target.value);
+        this.setState({
+            busqueda: e.target.value
+        });
+    }
+    state = {
+        busqueda : ""
+    }
     render(){
         return (
             <div className="container">
@@ -17,11 +30,11 @@ class PageHome extends React.Component{
                                     name="busqueda" 
                                     type="search" 
                                     placeholder="Banda"  
-                                    onChange={this.props.onChange}
+                                    onChange={this.onChange}
                                 />
                             </div>
                             <div className="actions">
-                                <button className="btng">Search Similar Artist</button>
+                                <button className="btng" type="submit">Search Similar Artist</button>
                                 <button className="btng">EscuelaDevRock</button>
                             </div>
                         </form>
